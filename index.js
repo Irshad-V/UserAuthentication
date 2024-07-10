6a3609a6fe5c1db1b6bab7a25393522e9a573734
Irshad-V <irshadvtl272@gmail.com>
2024-07-10 14:18:10 +0530

    user authentication method two type 1 session and cookie 2 ejs both method is simple for logged user privacy

--- index.js
+++ index.js
@@ -5,46 +5,48 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const authMiddleware = require('./views/middlware')
+// const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser('secret'));
+
 
 app.use(session({
   secret: 'secret',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // Set to true if using HTTPS
+  cookie: { secure: false } 
 }));
 
-// Middleware to protect routes
+function authMiddleware(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
+
+// Middleware to protect routes
 const dummyUser = {
   email: 'user@example.com',
   password: 'password123'
 };
-
 // Route for rendering the home page
 app.get('/success-page', authMiddleware, (req, res) => {
   try {
     console.log(req.session.user);
     res.render('index', { user: req.session.user });
-
   } catch (err) {
     console.log(err);
     console.log("success error");
   }
 })
-
 app.get('/login', (req, res) => {
-  console.log("okay");
   res.render('login');
 });
-
 // Route for handling login form submission
 app.post('/login', (req, res) => {
   console.log("Received login request");
@@ -52,12 +54,10 @@ app.post('/login', (req, res) => {
   console.log("Request Body:", req.body);
   const { email, password } = req.body;
 
-
   if (email === dummyUser.email && password === dummyUser.password) {
     req.session.user = { email };
-    res.cookie('userEmail', email, { signed: true });
+    
     res.redirect('/success-page')
-
 
   } else {
     res.status(401).json({ message: 'Invalid email or password' });
--- views/middlware.js
+++
@@ -1,9 +0,0 @@
- function authMiddleware(req, res, next) {
-    if (req.session.user) {
-      next();
-    } else {
-      res.redirect('/login'); // Redirect to login page if not logged in
-    }
-  }
- 
-  module.exports = authMiddleware;
